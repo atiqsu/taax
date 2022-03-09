393c257aafc626bc33a5a0166b6d0688aaee797b
Md. Atiqur Rahman <atiqur.su@gmail.com>
2022-03-09 09:28:53 +0600

    #updated

--- src/views/PageArTest.vue
+++ src/views/PageArTest.vue
@@ -1,13 +1,19 @@
 <template>
   <el-form ref="formRef" :model="myStore" label-width="150px">
 
-    <el-form-item label="Salary 0">
-      <el-input-number v-model="form.name"></el-input-number>
+    <el-form-item label="Tax year">
+      <el-select v-model="myStore.fiscalYear" placeholder="please select your tax year">
+        <el-option v-for="n in 15" :key="currentYear-n" :label="makeLabel(currentYear, n)" :value="makeLabel(currentYear, n)"></el-option>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="Detailed Salary">
+      <el-switch v-model="myStore.isOkay"></el-switch>
     </el-form-item>
 
     <el-form-item
         v-for="(salary, index) in myStore.salaries"
-        :key="salary"
+        :key="salary.key"
         :label="'Salary ' + (index + 1)"
         :rules="{
         required: true,
@@ -15,8 +21,8 @@
         trigger: 'blur',
       }"
     >
-      <el-input-number :model="salary"></el-input-number>
-      <el-button class="mt-2">Delete</el-button>
+      <el-input-number v-model="salary.val"></el-input-number>
+      <el-button class="mt-2" @click.prevent="delSalary(salary)">Delete</el-button>
     </el-form-item>
 
     <el-form-item>
@@ -24,24 +30,17 @@
     </el-form-item>
 
 
-    <el-form-item label="Bonus">
-      <el-input-number v-model="myStore.test"></el-input-number>
+    <el-form-item
+        v-for="(bonus, idx) in myStore.bonuses"
+        :key="bonus.key"
+        :label="'Bonus ' + (idx + 1)"
+    >
+      <el-input-number v-model="bonus.val"></el-input-number>
+      <el-button class="mt-2" @click.prevent="delBonus(bonus)">Delete</el-button>
     </el-form-item>
 
     <el-form-item>
-      <el-button type="warning" @click="addMoreSalary">Add Bonus</el-button>
-    </el-form-item>
-
-
-    <el-form-item label="Tax year">
-      <el-select v-model="myStore.fiscalYear" placeholder="please select your tax year">
-        <el-option v-for="n in 15" :key="currentYear-n" :label="makeLabel(currentYear, n)" :value="makeLabel(currentYear, n)"></el-option>
-      </el-select>
-    </el-form-item>
-
-
-    <el-form-item label="Some opt">
-      <el-switch v-model="myStore.isOkay"></el-switch>
+      <el-button type="warning" @click="addMoreBonus">Add Bonus</el-button>
     </el-form-item>
 
     <el-form-item label="Some other options">
@@ -71,15 +70,15 @@ let makeLabel: (year:number, decrease:number) => string =  (year:number, decreas
 
 const myStore = reactive({
   fiscalYear: makeLabel(currentYear.value, 1),
-  salaries: [55, 77],
-  bonuses: [],
+  salaries: [{key: Date.now(), val: 0}],
+  bonuses: [{key: Date.now(), val: 0}],
   selectedOpt: [],
   isOkay: true,
   test : 'atiq'
 });
 
 const totalSalary = computed(() => {
-  return myStore.salaries.length ? myStore.salaries.reduce((a, b)=>a+b, 0) : 0
+  return myStore.salaries.length ? myStore.salaries.reduce((a, b)=>a+b.val, 0) : 0
 });
 
 const someOptions = [
@@ -88,6 +87,27 @@ const someOptions = [
     'Option 3',
     'Option 4'
 ];
+
+const addMoreBonus = () => {
+  myStore.bonuses.push({
+    key: Date.now(),
+    val: 0,
+  })
+}
+
+const addMoreSalary = () => {
+  myStore.salaries.push({
+    key: Date.now(),
+    val: 0,
+  })
+}
+
+const delSalary = (item) => {
+  const index = myStore.salaries.indexOf(item)
+  if (index !== -1) {
+    myStore.salaries.splice(index, 1)
+  }
+}
 
 // do not use same name with ref
 const form = reactive({
